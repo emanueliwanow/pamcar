704b60bfb8bd0727f5cd1603ca23fd1eab4b1d18
SebastienBohn <sebohn1505@gmail.com>
2024-01-16 19:58:36 +0100

    Deleted useless code in TSP file

--- initial_guess/TSP.py
+++ initial_guess/TSP.py
@@ -29,10 +29,3 @@ def traveling_salesman_problem(doors):
             best_path = permutation
 
     return best_path, min_distance
-
-# Example for our problem :
-#doors_coordinates = [(0, 0), (3, 2), (2, 4), (6, 1)]
-#best_path, min_distance = traveling_salesman_problem(doors_coordinates)
-
-#print("Optimal path:", best_path)
-#print("Distance using this path:", min_distance)
