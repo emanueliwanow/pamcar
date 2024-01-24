a8354dc66a110a996a392b1a777d60365ba5d528
Sophia Moyen <sophiamoyen@gmail.com>
2024-01-24 23:10:37 +0100

    Add imaginary line door constraint, initial/end theta constraint, remove cos(delta) from ac

--- motion_planning/doors.py
+++ motion_planning/doors.py
@@ -2,22 +2,29 @@ import numpy as np
 import matplotlib.pyplot as plt
 from math import *
 """EDIT HERE THE DOORS YOU WANT
-"""
+
 # Parallel doors
 doors = {'A':[(3,5),(3,4)],
          'B':[(8,5),(8,4)],
          'C':[(8,2),(8,1)],
          'D':[(3,2),(3,1)]}
-"""
 
+"""
+"""
 # Scenario 1
 doors = {'A':[(2,6),(3,5)],
          'B':[(6,7),(6,6)],
          'C':[(10,5),(11,6)],
          'D':[(7,2),(7,1)]}
 
-
+"""
 # Scenario 3
+doors = {'A':[(3,3),(4,4)],
+         'B':[(3,-3),(4,-4)],
+         'C':[(-3,-3),(-4,-4)],
+         'D':[(-3,3),(-4,4)]}
+
+"""
 doors = {'A':[(3,3),(4,4)],
          'B':[(3,-3),(4,-4)],
          'C':[(-3,-3),(-4,-4)],
@@ -52,5 +59,3 @@ for key, value in doors.items():
 
     i += 1
     j += 2
-
-
--- motion_planning/opc_problem_car.py
+++ motion_planning/opc_problem_car.py
@@ -7,20 +7,20 @@ from pylab import plot, step, figure, legend, show, spy, title, xlabel, ylabel, 
 import matplotlib.pyplot as plt
 
 
-# Optimization problem
+""" ---- Optimization problem ---- """
 opti = casadi.Opti()
 
-# Parameters
+""" ---- Parameters ---- """
 intervals = 20 # Number of control intervals per phase
 n_phases = len(doors)+1 # Number of piecewise trajectories between doors and start/end point
 N = n_phases*intervals
 L = 0.16 # Length of the car
 delta_min = -pi/4# Minimum steering angle
 delta_max = pi/4 # Maximum steering angle
-a_min = -0.1 # Minimum acceleration
-a_max = 0.1 # Maximum acceleration
+a_min = -0.3 # Minimum acceleration
+a_max = 0.3 # Maximum acceleration
 v_max = 1 # Maximum velocity
-ac_max = 0.01 # Maximum centripetal acceleration
+ac_max = 0.3 # Maximum centripetal acceleration
 x_min = -20 # Min Boundary x of the track
 y_min = -20 # Min Boundary y of the track
 x_max = 20 # Max Boundary x of the track
@@ -28,28 +28,38 @@ y_max = 20 # Max Boundary y of the track
 
 # Defining the intermediate states
 X_waypoints = np.zeros(((4,len(doors))))
-for i in range(len(X_waypoints)):
+for i in range(len(X_waypoints[0])):
     X_waypoints[:,i] = [middle_points[0,i],
                        middle_points[1,i],
                        middle_points_theta[0,i],
                        0.1]
 
-# State variables X
+""" ---- State variables X ---- """
 X = opti.variable(4,N+1)
 x = X[0,:]                # x position
 y = X[1,:]                # y position
 theta = X[2,:]            # heading angle
 v = X[3,:]                # velocity
 
-# Control variables U
+""" ---- Control variables U ---- """
 U = opti.variable(2,N+1)
 delta = U[0,:]            # steering angle
 a = U[1,:]                # acceleration
 
-# Cost function
+""" ---- Other variables ---- """
 T = opti.variable()       # time to be minimized
 
-# Optimization problem
+letter_list = []
+for letter in doors:
+   """
+   Generates an optimization variable for each doors that
+   determines the point between the gates through which the car
+   should pass to ensure an optimal trajectory
+   """
+   locals()[letter] = opti.variable() 
+   exec("letter_list.append({})".format(letter))
+
+""" ---- Cost function ---- """
 opti.minimize(T) # Minimize the time to complete the trajectory
 
 x1 = MX.sym('x1')
@@ -77,7 +87,7 @@ x4dot = u2
 Xdot = vertcat(x1dot,x2dot,x3dot,x4dot)
 
 
-# ODE right handside function
+""" ---- ODE right handside function ---- """
 f = Function('f', [X_func,U_func],[Xdot])
 M = 4
 dt = T/N # length of the control interval
@@ -91,40 +101,45 @@ for k in range(N): # loop over control intervals
    opti.subject_to(X[:,k+1]==x_next) # close the gaps
    
 
-"""Boundary Conditions"""
+""" ----- Boundary Conditions ----- """
 opti.subject_to(x[0]==0) # Start x position
 opti.subject_to(y[0]==0) # Start y position
 opti.subject_to(v[0]==0) # Start v
-#opti.subject_to(theta[0]==pi/2) # Start theta position
+opti.subject_to(theta[0]==pi/2) # Start theta position
 
 opti.subject_to(x[N]==0) # End x position
 opti.subject_to(y[N]==0) # End y position
 opti.subject_to(v[N]==0) # End v
+opti.subject_to(theta[N]==pi/2) # Endtheta position
 
-for i in range(len(X_waypoints)):
-   i += 1
+for letter in letter_list:
+   opti.subject_to(opti.bounded(0,letter,1))
+
+i = 1
+for key,value in doors.items():
    #opti.subject_to(X[:,int((i*N)/n_phases)]==X_waypoints[:,i-1]) # Intermediate states
-   opti.subject_to(X[0,int((i*N)/n_phases)]==X_waypoints[0,i-1]) # x
-   opti.subject_to(X[1,int((i*N)/n_phases)]==X_waypoints[1,i-1]) # y
-   opti.subject_to(X[3,int((i*N)/n_phases)]==X_waypoints[3,i-1]) # v
+   opti.subject_to(X[0,int((i*N)/n_phases)]==value[0][0]+letter_list[i-1]*(value[1][0]-value[0][0])) # x
+   opti.subject_to(X[1,int((i*N)/n_phases)]==value[0][1]+letter_list[i-1]*(value[1][1]-value[0][1])) # y
+   #opti.subject_to(X[3,int((i*N)/n_phases)]==X_waypoints[3,i-1]) # v
+   i += 1
 
 
-"""Inequality Constraints"""
+""" ----- Inequality Constraints -----"""
 opti.subject_to(opti.bounded(delta_min,delta,delta_max)) # Limit on steering angle
-opti.subject_to(opti.bounded(a_min,a,a_max)) # Limit on acceleration
-opti.subject_to(v<=v_max) # Limit on velocity
-opti.subject_to(v>=0) # Velocity is greater or equal than zero
-opti.subject_to(T>=0) # Time must be positive
+opti.subject_to(opti.bounded(a_min,a,a_max))             # Limit on acceleration
+opti.subject_to(v<=v_max)                                # Limit on velocity
+opti.subject_to(v>=0)                                    # Velocity is greater or equal than zero
+opti.subject_to(T>=0)                                    # Time must be positive
 
-ac = lambda v,delta,L: (v**2*tan(delta))/(L*cos(delta)) # Centripetal acceleration definition
+ac = lambda v,delta,L: (v**2*tan(delta))/(L)  # Centripetal acceleration definition
 opti.subject_to(ac(v,delta,L)<=ac_max)
 
-"""Environment Constraints"""
+""" ----- Environment Constraints ----- """
 opti.subject_to(opti.bounded(x_min,x,x_max)) # Boundary x of the track
 opti.subject_to(opti.bounded(y_min,y,y_max)) # Boundary y of the track
 
 
-# Initial guess for state variables
+""" ----- Initial Guess for State Variables ----- """
 x_guess = np.zeros(N + 1)
 y_guess = np.zeros(N + 1)
 x_guess[0:int(N/(n_phases))] = np.linspace(0, X_waypoints[0][0], int(N/n_phases))  # Linear interpolation to first door
@@ -140,21 +155,23 @@ for i in range(len(doors)-1):
 x_guess[len(doors)*int(N/(n_phases)):N+1] = np.linspace(X_waypoints[0][i],0,int(N/n_phases)+1) # Return to starting point
 y_guess[len(doors)*int(N/(n_phases)):N+1] = np.linspace(X_waypoints[1][i],0,int(N/n_phases)+1) # Return to starting point
 
-theta_guess = np.zeros(N + 1)  # Assuming initial heading towards door
-v_guess = np.ones(N + 1)  # Starting with moderate velocity
-T_guess = 10  # Rough estimate of total time
+theta_guess = np.zeros(N + 1)    # Assuming initial heading towards door
+v_guess = np.ones(N + 1)         # Starting with moderate velocity
+
+""" ----- Initial Guess for Other Variables ----- """
+T_guess = 10                     # Rough estimate of total time
 
 
-# Setting initial guesses
+""" ---- Setting initial guesses ---- """
 opti.set_initial(X[:, :], np.vstack((x_guess, y_guess, theta_guess, v_guess)))
 opti.set_initial(T, T_guess)
 
-# Solver
+""" ---- Solver ---- """
 opti.solver('ipopt')
 sol = opti.solve()
 
-# Plot Graphics
 
+""" ---- Plotting ---- """
 plt.figure()
 #plt.plot(sol.value(x),label="x")
 #plt.plot(sol.value(y),label="y")
@@ -184,3 +201,5 @@ plt.ylabel("Position y")
 plt.legend(loc="upper left")
 
 plt.show()
+
+print(sol.value(T))
